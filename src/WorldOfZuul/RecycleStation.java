7dbe5050383a717bf8695e160f6a8540740a8150
Oliverxll <bibern@live.dk>
2022-11-01 21:55:46 +0100

    Create RecycleStation.java

---
+++ src/WorldOfZuul/RecycleStation.java
@@ -0,0 +1,10 @@
+package WorldOfZuul;
+
+public class RecycleStation extends Room
+{
+
+    public RecycleStation(String description)
+    {
+        super(description + "\nThere is a recycle station here.");
+    }
+}
