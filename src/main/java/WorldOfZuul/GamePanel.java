05b84b325af4d7cddd33b44ff17ce17f32ea21fd
Oliverxll <bibern@live.dk>
2022-11-22 15:39:54 +0100

    Update GamePanel.java

--- src/main/java/WorldOfZuul/GamePanel.java
+++ src/main/java/WorldOfZuul/GamePanel.java
@@ -3,5 +3,10 @@ package WorldOfZuul;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    final int UNIT_SIZE_UNSCALED = 16; //16x16 Tile
+    final int SCALE = 4;
+    final int UNIT_SIZE = UNIT_SIZE_UNSCALED * SCALE; // Make it 64-bit
 
+    
 }
