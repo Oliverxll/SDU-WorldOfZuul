52aa8561730cf92d5c808904dd22e2665ef3cd90
LauraMarie01 <115552720+LauraMarie01@users.noreply.github.com>
2022-11-24 11:26:31 +0100

    Set første video og fulgt den. Der er lavet kommentare undervejs

--- src/main/java/WorldOfZuul/GamePanel.java
+++ src/main/java/WorldOfZuul/GamePanel.java
@@ -1,12 +1,45 @@
 package WorldOfZuul;
 
 import javax.swing.JPanel;
+import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {// GamePanel is a subclass to JPanel
+
     // SCREEN SETTINGS
     final int UNIT_SIZE_UNSCALED = 16; //16x16 Tile
     final int SCALE = 4;
-    final int UNIT_SIZE = UNIT_SIZE_UNSCALED * SCALE; // Make it 64-bit
+    final int UNIT_SIZE = UNIT_SIZE_UNSCALED * SCALE; // Make it 64-bit. This will make things scale up n size it fit screen
 
-    
+
+    //We decide the size of the game screen
+    final int maxScreenCol = 16; //Horizontally
+    final int maxScreenRow = 12; // Vertically
+
+    final int ScreenWidth = UNIT_SIZE * maxScreenCol; // 64* 16 = 1024 pixels
+    final int ScreenHeight = UNIT_SIZE * maxScreenRow; //64 * 12 = 768 pixels
+    //The size of our game screen will then be 1024 x 768 pixels
+
+
+    //When we call this thread it will automatically call the run method further below
+    Thread gameThread; // Makes it possible to start and stop a game. To use Thread we implement "Runnable" to this class
+
+    //Constructor
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(ScreenWidth, ScreenHeight));//Sets the size of this class
+        this.setBackground(Color.BLACK); //Sets the background color to black
+    }
+
+
+    //We initiate thread. "(this)" is the class gamePanel
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start(); //It will automatically call the run method
+    }
+
+
+    //This is a method that we get recommended when implementing "Runnable" to the class
+    @Override
+    public void run() {
+
+    }
 }
--- src/main/java/WorldOfZuul/Main.java
+++ src/main/java/WorldOfZuul/Main.java
@@ -1,10 +1,30 @@
 package WorldOfZuul;
 
 
+import javax.swing.*;
+
 public class Main{
 
 
     public static void main(String[] args) {
+
+        JFrame window = new JFrame(); //Makes the JFrame window. This is our main window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Makes the game close properly when clicking (X)
+        window.setResizable(false);// The user can't change the size of the window
+        window.setTitle("World of Garbage"); //Set the title of the window
+
+       // We add the GamePanel class to main
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); // "pack()" makes the window the size that we set in GamePanel
+
+
+
+        window.setLocationRelativeTo(null);// Places the window in the center of the screen
+        window.setVisible(true);//So we can se window
+
+
+
 
     }
 }
