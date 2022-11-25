991e58b6a3bce2d78a3bbc316d60ecc5dff11e8c
Oliverxll <bibern@live.dk>
2022-11-25 15:33:08 +0100

    Update Player.java
    More constructors, moved values, fixed imageloader, refactored code, renamed variables

--- src/main/java/WorldOfZuul/Player.java
+++ src/main/java/WorldOfZuul/Player.java
@@ -1,55 +1,62 @@
 package WorldOfZuul;
 
-import WorldOfZuul.DataTypes.IntVector2;
 import WorldOfZuul.DataTypes.Position;
 
-public class Player
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity
 {
-    public Position position;
-
-    public Player() {
-        this.position = new Position();
-    }
-
-    public Player(Position position) {
-        this.position = position;
-    }
-
-    public int speed;
+    public int moveSpeed;
     GamePanel gamePanel;
     KeyHandler keyhandler;
 
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
-    public String direction;
+    String direction;
 
-    public Player (GamePanel gamePanel, KeyHandler keyhandler){
+    public Player(GamePanel gamePanel, KeyHandler keyhandler)
+    {
         this.gamePanel = gamePanel;
         this.keyhandler = keyhandler;
         setDefaultValues();
         getPlayerImage();
-
     }
 
-    public void setDefaultValues (){
-        position.x = 80;
-        position.y = 80;
-        speed = 4;
+    public Player()
+    {
+        this.position = new Position();
+    }
+
+    public Player(Position position)
+    {
+        this.position = position;
+    }
+
+    public void setDefaultValues()
+    {
+        position = new Position(80, 80);
+        moveSpeed = 4;
         direction = "down";
     }
 
 
-    public void getPlayerImage(){
-        try{
-            up1= ImageIO.read(getClass().getResourceAsStream("/character/boy_up_1.png"));
-            up2= ImageIO.read(getClass().getResourceAsStream("/character/boy_up_2.png"));
-            down1= ImageIO.read(getClass().getResourceAsStream("/character/boy_down_1.png"));
-            down2= ImageIO.read(getClass().getResourceAsStream("/character/boy_down_2.png"));
-            left1= ImageIO.read(getClass().getResourceAsStream("/character/boy_left_1.png"));
-            left2= ImageIO.read(getClass().getResourceAsStream("/character/boy_left_2.png"));
-            right1= ImageIO.read(getClass().getResourceAsStream("/character/boy_right_1.png"));
-            right2= ImageIO.read(getClass().getResourceAsStream("/character/boy_right_2.png"));
+    public void getPlayerImage()
+    {
+        try
+        {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/sprites/player/boy_right_2.png"));
 
-        }catch (IOException e){
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
@@ -57,42 +64,45 @@ public class Player
     public void update (){
         if (keyhandler.upPressed == true) {
             direction = "up";
-            position.y -= speed;
+            position.y -= moveSpeed;
         } else if (keyhandler.downPressed == true) {
             direction ="down";
-            position.y += speed;
+            position.y += moveSpeed;
         } else if (keyhandler.leftPressed == true) {
             direction = "left";
-            position.x -= speed;
+            position.x -= moveSpeed;
         } else if (keyhandler.rightPressed == true) {
             direction = "right";
-            position.x += speed;
+            position.x += moveSpeed;
 
         }
     }
     public void draw(Graphics2D g2){
         //This makes a white rectangle.
-        //g2.setColor(Color.white);
+        g2.setColor(Color.white);
 
         //This is the coordinates and size
-       // g2.fillRect(x, y, gp.UNIT_SIZE, gp.UNIT_SIZE);
+        g2.fillRect(position.x, position.y, gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE);
 
-        BufferedImage image = null;
+        BufferedImage image = down1;
         switch (direction){
-            case"up":
-                image=up1;
-                break;
-            case "down":
-                image=down1;
-                break;
-            case "left":
-                image=left1;
-                break;
-            case "right":
-                image=right1;
-                break;
+            case "up" -> {
+                image = up1;
+            }
+            case "down" ->
+            {
+                image = down1;
+            }
+            case "left" ->
+            {
+                image = left1;
+            }
+            case "right" ->
+            {
+                image = right1;
+            }
         }
-        g2.drawImage(image, x, y, gp.UNIT_SIZE, gp.UNIT_SIZE, null);
+        g2.drawImage(image, position.x, position.y, gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE, null);
 
     }
 }
