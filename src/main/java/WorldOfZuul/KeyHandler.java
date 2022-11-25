f67c6993cc4e68bf3ca5b97c01164ed3bba4da6b
Oliverxll <bibern@live.dk>
2022-11-25 15:33:25 +0100

    Added WASD to control scheme

--- src/main/java/WorldOfZuul/KeyHandler.java
+++ src/main/java/WorldOfZuul/KeyHandler.java
@@ -2,8 +2,9 @@ package WorldOfZuul;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.security.Key;
 
-public class KeyHandler implements KeyListener {//keylistener receives keyboard events.
+public class KeyHandler implements KeyListener { //keylistener receives keyboard events.
     //It then wants us to implement the methods: keyTyped, keyPressed and keyReleased
 
     public boolean upPressed, downPressed, leftPressed, rightPressed;
@@ -16,16 +17,16 @@ public class KeyHandler implements KeyListener {//keylistener receives keyboard 
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode(); //It returns the integer key code associated with the key pressed
 
-        if (code == KeyEvent.VK_UP) {
+        if (code == KeyEvent.VK_UP || code == KeyEvent.VK_W) {
             upPressed = true;
         }
-        if (code == KeyEvent.VK_DOWN) {
+        if (code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S) {
             downPressed = true;
         }
-        if (code == KeyEvent.VK_LEFT) {
+        if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A) {
             leftPressed = true;
         }
-        if (code == KeyEvent.VK_RIGHT) {
+        if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
             rightPressed = true;
         }
 
@@ -35,21 +36,24 @@ public class KeyHandler implements KeyListener {//keylistener receives keyboard 
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_UP) {
+        if (code == KeyEvent.VK_UP || code == KeyEvent.VK_W) {
             upPressed = false;
         }
-        if (code == KeyEvent.VK_DOWN) {
+        if (code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S) {
             downPressed = false;
         }
-        if (code == KeyEvent.VK_LEFT) {
+        if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A) {
             leftPressed = false;
         }
 
-        if (code == KeyEvent.VK_RIGHT) {
+        if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
             rightPressed = false;
         }
 
     }
 
+    enum MoveDirection {
+        UP, DOWN, LEFT, RIGHT, NOTHING
+    }
 
 }
