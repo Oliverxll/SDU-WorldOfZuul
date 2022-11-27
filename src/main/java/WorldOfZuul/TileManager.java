f861c8a849617cc5e786587d1970a0436569a443
Oliverxll <bibern@live.dk>
2022-11-28 00:05:18 +0100

    Create TileManager.java
    That can load the map and draw it for us

---
+++ src/main/java/WorldOfZuul/TileManager.java
@@ -0,0 +1,124 @@
+package WorldOfZuul;
+
+import WorldOfZuul.DataTypes.Tile;
+import WorldOfZuul.Utility.ImageLoader;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TileManager
+{
+    GamePanel gamePanel;
+    Tile[] tiles;
+    int[][] mapTileNumber;
+
+    public TileManager(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+
+        tiles = new Tile[10];
+        mapTileNumber = new int[gamePanel.maxWorldCol][gamePanel.maxWorldRow];
+
+        getTileImage();
+        loadMap("/maps/map.txt");
+    }
+
+    public void getTileImage() {
+        try
+        {
+            tiles[0] = new Tile();
+            tiles[0].image = ImageLoader.getInstance().Load("/sprites/tiles/blank.png", gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE);
+            tiles[0].collision = true;
+
+            tiles[1] = new Tile();
+            tiles[1].image = ImageLoader.getInstance().Load("/sprites/tiles/road_0.png", gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE);
+            tiles[1].collision = false;
+
+            tiles[2] = new Tile();
+            tiles[2].image = ImageLoader.getInstance().Load("/sprites/tiles/road_1.png", gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE);
+            tiles[2].collision = false;
+
+            tiles[3] = new Tile();
+            tiles[3].image = ImageLoader.getInstance().Load("/sprites/tiles/sidewalk_0.png", gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE);
+            tiles[3].collision = false;
+
+            tiles[4] = new Tile();
+            tiles[4].image = ImageLoader.getInstance().Load("/sprites/tiles/wall_0.png", gamePanel.UNIT_SIZE, gamePanel.UNIT_SIZE);
+            tiles[4].collision = true;
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadMap(String path) {
+        try {
+            InputStream inputStream = getClass().getResourceAsStream(path);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gamePanel.maxWorldCol && row < gamePanel.maxWorldRow)
+            {
+                String line = bufferedReader.readLine();
+
+                while (col < gamePanel.maxWorldCol) {
+
+                    String[] numbers = line.split(" ");
+
+                    int number = Integer.parseInt(numbers[col]);
+
+                    mapTileNumber[col][row] = number;
+                    col++;
+                }
+
+                if (col == gamePanel.maxWorldCol) {
+                    col = 0;
+                    row++;
+                }
+            }
+
+            bufferedReader.close();
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+
+        int worldCol = 0;
+        int worldRow = 0;
+
+        while (worldCol < gamePanel.maxWorldCol && worldRow < gamePanel.maxWorldRow)
+        {
+            int tileNumber = mapTileNumber[worldCol][worldRow];
+
+            int worldX = worldCol * gamePanel.UNIT_SIZE;
+            int worldY = worldRow * gamePanel.UNIT_SIZE;
+            int screenX = worldX - gamePanel.player.position.x + gamePanel.player.screenPos.x;
+            int screenY = worldY - gamePanel.player.position.y + gamePanel.player.screenPos.y;
+
+            // This ugly if statement is basically if the tile is outside the players view, we won't render it.
+            if (worldX + gamePanel.UNIT_SIZE > gamePanel.player.position.x - gamePanel.player.screenPos.x &&
+                worldX - gamePanel.UNIT_SIZE < gamePanel.player.position.x + gamePanel.player.screenPos.x &&
+                worldY + gamePanel.UNIT_SIZE > gamePanel.player.position.y - gamePanel.player.screenPos.y &&
+                worldY - gamePanel.UNIT_SIZE < gamePanel.player.position.y + gamePanel.player.screenPos.y)
+            {
+                g2.drawImage(tiles[tileNumber].image, screenX, screenY, null);
+            }
+
+            worldCol++;
+
+            if (worldCol == gamePanel.maxWorldCol) {
+                worldCol = 0;
+                worldRow++;
+            }
+        }
+    }
+}
