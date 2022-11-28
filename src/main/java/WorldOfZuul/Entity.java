9b36fbe3cc3c8000959543ddb2dbc868ef024e7c
Oliverxll <bibern@live.dk>
2022-11-28 01:05:16 +0100

    Update Entity.java
    Added more variables

--- src/main/java/WorldOfZuul/Entity.java
+++ src/main/java/WorldOfZuul/Entity.java
@@ -2,7 +2,13 @@ package WorldOfZuul;
 
 import WorldOfZuul.DataTypes.Position;
 
+import java.awt.*;
+
 public class Entity
 {
     public Position position;
+    public Rectangle collider;
+    public boolean collision = false;
+    public String direction;
+    public int moveSpeed;
 }
